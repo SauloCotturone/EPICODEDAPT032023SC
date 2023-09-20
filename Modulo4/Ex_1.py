8df4925cb6025f0aafdad05e167db0682dc7d85f
Saulo Cotturone <saulogoodman21@gmail.com>
2023-09-20 19:29:28 +0200

    Exercise 1

---
+++ Modulo4/Ex_1.py
@@ -0,0 +1,26 @@
+
+### Ex_1
+
+studenti = 25
+print("Gli studenti iniziali sono", studenti)
+nuovi_studenti = 3
+studenti += nuovi_studenti
+studenti_tot = studenti
+print("Gli studenti finali sono", studenti_tot)
+
+
+lista = [1, 2, 3, 4, 5]
+print(lista)
+
+lista_film = ["Non è un paese per vecchi", "Il cavaliere oscuro", "Il signore degli anelli"]
+print(lista_film)
+
+
+
+
+
+
+
+
+
+
